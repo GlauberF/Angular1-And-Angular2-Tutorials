e6cc26fddb2d48e50d3da0729fc36b1b0e2d5493
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-04-14 21:47:00 +0200

    more...working on directives

--- Angular1-Tutorial/05_Directives/end/app/home.controller.js
+++ Angular1-Tutorial/05_Directives/end/app/home.controller.js
@@ -27,8 +27,67 @@
         }
     }
 
+    function secondController() {
+        var vm = this;
+        vm.text = "This comes from the second controller";
+        vm.text2 = "This is for the isolated directives...."
+    }
+
+    function myDirective() {
+        return {
+            restrict: "AEC",
+            template: "<h4>Hellooooo!</h4>"
+        };
+    }
+
+    function myDirective2() {
+        return {
+            restrict: "AEC",
+            template: "<h4>{{vm.text}}</h4>"
+        };
+    }
+
+    function isolatedScopeDirective() {
+        return {
+            restrict: "AEC",
+            scope: {
+                givenText: '=text'
+            },
+            template: "<h4>Isolated: {{givenText}}</h4>"
+        };
+    }
+
+    function isolatedScopeDirectiveWithLink() {
+        return {
+            restrict: "AEC",
+            link: function (scope, element, attributes, controller) {
+                element.html(scope.vm.text);
+                element.css("background-color", "#ffff00");
+            }
+        };
+    }
+    
+     function isolatedScopeDirectiveWithController() {
+        return {
+            restrict: "AEC",
+            link: function (scope, element, attributes, controller) {
+                element.css("background-color", "#ffff00");
+            },
+            controller: function ($scope) {
+                $scope.inside = "This is inside the directive";
+            },
+            template: "{{inside}}"
+        };
+    }
+
     angular
         .module("AngularJsDemoApp")
-        .controller("homeController", homeController);
+        .controller("homeController", homeController)
+        .controller("secondController", secondController)
+        .directive("mySecondDirective", myDirective2)
+        .directive("isolatedScopeDirective", isolatedScopeDirective)
+        .directive("isolatedScopeDirectiveWithController", isolatedScopeDirectiveWithController)
+        .directive("isolatedScopeDirectiveWithLink", isolatedScopeDirectiveWithLink)
+        .directive("myDirective", myDirective);
 
 } ());
