a091c2046fa397c267ba44d5dc2455cf83d950c0
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-04-24 21:12:23 +0200

    testing and route module

---
+++ Angular1-Tutorial/10_Routing/end_v1/app/application.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    "use strict";
+
+    var app = angular.module("AngularJsDemoApp", ['ngRoute']);
+
+    app.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'app/home.template.html',
+                    controller: 'homeController',
+                    controllerAs: 'vm'
+                }).
+                when('/about', {
+                    templateUrl: 'app/about.template.html',
+                    controller: 'aboutController',
+                    controllerAs: 'vm'
+                }).
+                when('/details/:id', {
+                    templateUrl: 'app/homeDetails.template.html',
+                    controller: 'homeDetailsController',
+                    controllerAs: 'vm'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+        }]);
+} ());
