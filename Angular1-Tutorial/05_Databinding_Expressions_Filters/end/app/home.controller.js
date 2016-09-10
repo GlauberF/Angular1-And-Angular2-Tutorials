c26178aba12f9a475aeabe19a05b6524e1fc519c
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-09-11 01:32:07 +0200

    added some filter demos

--- Angular1-Tutorial/05_Databinding_Expressions_Filters/end/app/home.controller.js
+++ Angular1-Tutorial/05_Databinding_Expressions_Filters/end/app/home.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     "use strict";
 
@@ -8,23 +8,35 @@
 
         $scope.value = "This is my first app";
 
-        $scope.clickMe = function() {
+        $scope.clickMe = function () {
             alert("Clicked");
         }
 
-        $scope.add = function(a, b) {
+        $scope.add = function (a, b) {
             return a + b;
         }
 
         $scope.foodItem = { calories: 500 }
 
-        $scope.foodItems = ["Burger", "Pommes", "Nudeln"];
+       $scope.foodItems = ["Burger", "Pommes", "Nudeln"];
 
         $scope.dateForFilter = new Date();
     }
 
+    function myCustomFilter() {
+        return function (param1, param2) {
+            console.log("param1 " + param1);
+            console.log("param2 " + param2);
+
+            if (param1 && param2) {
+                return "Hello";
+            }
+        };
+    }
+
     angular
         .module("AngularJsDemoApp")
-        .controller("homeController", homeController);
+        .controller("homeController", homeController)
+        .filter('myCustomFilter', myCustomFilter);
 
 } ());
