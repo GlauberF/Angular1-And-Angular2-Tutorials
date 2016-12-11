4fc0b44b57aebe962ae124bcb03eab0fb2bd68f1
FabianGosebrink <info@fabian-gosebrink.com>
2016-12-11 10:51:54 +0100

    added forms and validated form

--- Angular2-Tutorial/07_Forms/end/app/app.module.ts
+++ Angular2-Tutorial/07_Forms/end/app/app.module.ts
@@ -1,16 +1,22 @@
+import { IsNumberValidator } from './validators/isNumber.validator';
+import { IsInRangeValidator } from './validators/isInRange.validator';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
 
     declarations: [
         AppComponent,
-        NavigationComponent
+        NavigationComponent,
+        IsNumberValidator,
+        IsInRangeValidator
     ],
 
     bootstrap: [
