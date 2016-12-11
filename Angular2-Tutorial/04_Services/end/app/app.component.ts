28c730114b0b6bb78e8c45f4eee0036d8f714dc5
FabianGosebrink <info@fabian-gosebrink.com>
2016-12-11 09:47:30 +0100

    added http to services

--- Angular2-Tutorial/04_Services/end/app/app.component.ts
+++ Angular2-Tutorial/04_Services/end/app/app.component.ts
@@ -1,5 +1,8 @@
+import { FoodItem } from './models/foodItem';
+import { FoodDataService } from './shared/services/item.dataservice';
 import { CalculatorService } from './shared/services/calculator.service';
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -11,12 +14,18 @@ export class AppComponent implements OnInit {
 
     public result: number;
     private calculatorService: CalculatorService;
+    private itemDataService: FoodDataService;
+    private items: FoodItem[];
 
-    constructor() {
+    constructor(private http: Http) {
         this.calculatorService = new CalculatorService();
+        this.itemDataService = new FoodDataService(http);
     }
 
     ngOnInit() {
         this.result = this.calculatorService.add(1, 3);
+        this.itemDataService.GetAllFood().subscribe((result) => {
+            this.items = result;
+        });
     }
 }
